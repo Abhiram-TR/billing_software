7ce1265cb40682a3ea67ec2103d5ac2b156cb072
jeevjacobgeorge <jeev@J4Hp>
2024-07-18 16:10:31 +0530

    fixed migration error

--- billing_app/migrations/0001_initial.py
+++ billing_app/migrations/0001_initial.py
@@ -11,15 +11,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='StockIn',
-            fields=[
-                ('m_id', models.AutoField(primary_key=True, serialize=False)),
-                ('material', models.CharField(max_length=255)),
-                ('quantity', models.PositiveIntegerField()),
-                ('taxable_value', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('tax_percentage', models.DecimalField(decimal_places=2, max_digits=5)),
-                ('date', models.DateField()),
-            ],
-        ),
+       
     ]
